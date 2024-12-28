28def3aadbc6e39192d4a7e4556fc97ad696d839
Apzx <pzx110294@student.poznan.merito.pl>
2024-12-28 15:29:23 +0100

    Display best rated books on home page

--- Serwis Książkowy/Controllers/HomeController.cs
+++ Serwis Książkowy/Controllers/HomeController.cs
@@ -1,21 +1,35 @@
 using Microsoft.AspNetCore.Mvc;
 using Serwis_Książkowy.Models;
 using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using Serwis_Książkowy.Data;
 
 namespace Serwis_Książkowy.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ApplicationDbContext dbContext)
         {
-            _logger = logger;
+            db = dbContext;
         }
 
         public IActionResult Index()
         {
             return View();
+            List<Book> books = db.Books
+                .Include(a => a.Author)
+                .Where(b => b.Rating < 5)
+                .OrderByDescending(b => b.Rating)
+                .Take(10)
+                .ToList();
+            ViewData["Header"] = "Best rated books";
+            return View(books);
+        }
+
+    
         }
 
         public IActionResult Privacy()
