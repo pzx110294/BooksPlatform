9f7a69eb63f7a53303c7dab1f37eb661f21f4c4f
Apzx <pzx110294@student.poznan.merito.pl>
2025-01-01 14:56:01 +0100

    cleanup

--- Serwis Książkowy/Helpers/BookQueryHelper.cs
+++ Serwis Książkowy/Helpers/BookQueryHelper.cs
@@ -11,9 +11,10 @@ public static class BookQueryHelper
 {
     public static (IQueryable<BookViewModel> Books, int TotalPages) GetBestRatedBooks(ApplicationDbContext context, int page, int pageSize, string? userId = null)
     {
-        int totalPages = GetTotalPages(context.Books, pageSize); 
+        IQueryable<Book> query = context.Books;
+        int totalPages = GetTotalPages(query, pageSize); 
             
-        var books = context.Books
+        var books = query
             .Include(a => a.Author)
             .Include(l => l.UserLibraries.Where(u => u.UserId == userId))
             .OrderByDescending(b => b.Rating)
@@ -27,17 +28,18 @@ public static class BookQueryHelper
     public static (IQueryable<BookViewModel> Books, int TotalPages) GetSearchedBooks(ApplicationDbContext context, string searchQuery, int page, int pageSize,
         string? userId = null)
     {
-        int totalPages = GetTotalPages(context.Books.Where(b =>
-            b.Author.Name.Contains(searchQuery) || 
+        IQueryable<Book> query = context.Books
+            .Where(b =>
+            b.Author.Name.Contains(searchQuery) ||
             b.Title.Contains(searchQuery) ||
-            b.Isbn == searchQuery)
-            , pageSize);
-        var books = context.Books
+            b.Isbn == searchQuery);
+        
+        int totalPages = GetTotalPages(query, pageSize);
+        
+        var books = query
             .Include(a => a.Author)
             .Include(l => l.UserLibraries.Where(u => u.UserId == userId))
             .OrderByDescending(b => b.Rating)
-            .Where(b => b.Author.Name.Contains(searchQuery) || b.Title.Contains(searchQuery) ||
-                        b.Isbn == searchQuery)
             .Select(book => MapToBookViewModel(book, userId))
             .Skip((page - 1) * pageSize)
             .Take(pageSize);
@@ -46,19 +48,27 @@ public static class BookQueryHelper
     public static (IQueryable<BookViewModel> Books, int TotalPages) GetUserBooks(ApplicationDbContext context, ClaimsPrincipal User, int page, int pageSize)
     {
         string userId = User.GetUserId();
+
+        IQueryable<UserLibrary> query = context.UserLibraries
+            .Where(u => u.UserId == userId);
         
-        int totalPages = GetTotalPages(context.UserLibraries.Where(u => u.UserId == userId), pageSize);
-        IQueryable<BookViewModel> books = context.UserLibraries
+        int totalPages = GetTotalPages(query, pageSize);
+        
+        IQueryable<BookViewModel> books = query
             .Include(b => b.Book)
             .ThenInclude(b => b.UserLibraries.Where(u => u.UserId == userId))
             .Include(a => a.Book.Author)
-            .Where(u => u.UserId == userId)
             .Select(book => MapToBookViewModel(book.Book, userId))
             .Skip((page - 1) * pageSize)
             .Take(pageSize);
         
         return (books, totalPages);
 
+    }
+    private static int GetTotalPages<T>(IQueryable<T> dbSet, int pageSize) where T : class
+    {
+        int totalBooks = dbSet.Count();
+        return (int)Math.Ceiling((float) totalBooks/ pageSize);
     }
 
     private static BookViewModel MapToBookViewModel(Book book, string? userId)
@@ -76,10 +86,5 @@ public static class BookQueryHelper
         };
     }
 
-    private static int GetTotalPages<T>(IQueryable<T> dbSet, int pageSize) where T : class
-    {
-        int totalBooks = dbSet.Count();
-        return (int)Math.Ceiling((float) totalBooks/ pageSize);
-    }
     
 }
